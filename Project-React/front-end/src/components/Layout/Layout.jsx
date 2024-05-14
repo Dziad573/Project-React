17e9e9ae669db4023f32877651e929990ec1a0d6
Dziad573 <krystiansmagacz05@gmail.com>
2024-05-14 20:39:41 +0200

    menu, content

--- Project-React/front-end/src/components/Layout/Layout.jsx
+++ Project-React/front-end/src/components/Layout/Layout.jsx
@@ -1,21 +1,27 @@
+import { CategoryMenu } from "../CategoryMenu/CategoryMenu"
 import { CurrencySelector } from "../CurrencySelector/CurrencySelector"
 import { Footer } from "../Footer/Footer"
 import { IconMenu } from "../IconMenu/IconMenu"
 import { Logo } from "../Logo/Logo"
 import { MainMenu } from "../MainMenu/MainMenu"
 import { TopBar } from "../TopBar/TopBar"
-export function Layout() {
+import { MainContent } from "../MainContent/MainContent"
+export function Layout({ children }) {
     return (
         <>
-            <TopBar>
-                <MainMenu></MainMenu>
-                <Logo></Logo>
-                <div>
-                    <CurrencySelector></CurrencySelector>
-                    <IconMenu></IconMenu>
-                </div>
-                
-            </TopBar>
+            <MainContent>
+                <TopBar>
+                    <MainMenu></MainMenu>
+                    <Logo></Logo>
+                    <div>
+                        <CurrencySelector></CurrencySelector>
+                        <IconMenu></IconMenu>
+                    </div>
+                    
+                </TopBar>
+                <CategoryMenu></CategoryMenu>
+                {children}
+            </MainContent>
             <Footer></Footer>
         </>
     )
